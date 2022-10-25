c6afa2419deba2ce0cdb8e37ddf17b1d246a9c22
WitchingHr <matt1494@gmail.com>
2022-10-25 13:27:41 -0400

    Add glow and click to clear

--- script.js
+++ script.js
@@ -12,11 +12,35 @@ function makeGrid(columns, rows) {
 
 makeGrid(16,16);
 
-// Event listener
+// Change border color
+container.addEventListener('mouseover', changeBorder);
+
+function changeBorder() {
+    divs.forEach(div => div.style.borderColor = 'rgb(26, 65, 86)');
+}
+
+// Change color
 const divs = document.querySelectorAll('.div');
-divs.forEach(div => div.addEventListener('mouseover', changeColor));
+divs.forEach(div => { 
+    div.addEventListener('mouseover', changeColor)
+    div.addEventListener('mouseover', removeTransition)
+});
 
 function changeColor(e) {
     e.target.style.background = 'black';
     e.target.style.borderColor = 'white';
+}
+
+// Clear screen
+container.addEventListener('click', clearScreen);
+
+function clearScreen() {
+    divs.forEach(div => {
+        div.classList.add('div-click');
+        div.style.background = 'none'
+    });
+}
+
+function removeTransition() {
+    divs.forEach(div => div.classList.remove('div-click'))
 }
